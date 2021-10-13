f1dd0e417fb4e7340b772401c4daf4c030f57627
inesmachado17 <ines.machado17@gmail.com>
2021-10-13 17:32:08 +0100

    added functions and for cycle

--- main.c
+++ main.c
@@ -20,6 +20,14 @@
 
 char EXTENSIONS[7][6] = {"pdf", "gif", "jpg", "png", "mp4", "zip", "html"};
 
+// void file_opt(char *filename);
+
+int file_check(char *filename);
+
+char *extract(char *filename);
+
+//char **file_list(char **args, int n_args);
+
 int main(int argc, char *argv[])
 {
     struct gengetopt_args_info args_info;
@@ -29,152 +37,179 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    // int num_files = args_info.file_given;
+    int num_files = args_info.file_given;
 
-    // char **files_list = NULL;
-    // files_list = MALLOC(sizeof(char *) * num_files);
+    char **files_list = NULL;
+    files_list = (char **)MALLOC(sizeof(char *) * num_files);
 
-    // for (int i = 0; i < num_files; i++)
+    int i;
+
+    for (i = 0; i < num_files; i++)
+    {
+        files_list[i] = (char *)MALLOC(sizeof(char) * (strlen(args_info.file_arg[i]) + 1));
+        strcpy(files_list[i], args_info.file_arg[i]);
+    }
+
+    for (i = 0; i < num_files; ++i)
+    {
+        char *filename = files_list[i];
+        if (file_check(filename) > 0)
+        {
+            continue;
+        }
+        char *extension = extract(filename);
+        DEBUG("%s", extension);
+    }
+
+    FREE(files_list);
+
+    // int fd_out;
+
+    // char *out_filename = "./out.txt";
+
+    // pid_t pid = fork();
+
+    // switch (pid)
     // {
-    //     char *filename = MALLOC(sizeof(char) * (strlen(args_info.file_arg[i]) + 1));
-    //     strcpy(filename, args_info.file_arg[i]);
-    //     FREE(filename);
+    // case -1:
+    //     ERROR(5, "fork failed\n");
+    //     break;
+    // case 0:
+
+    //     fd_out = open(out_filename, O_CREAT | O_WRONLY | O_TRUNC | O_SYNC, 0777);
+
+    //     if (fd_out == -1)
+    //     {
+    //         ERROR(3, "Failed to create output file\n");
+    //     }
+
+    //     dup2(fd_out, STDOUT_FILENO);
+
+    //     if (close(fd_out) == -1)
+    //     {
+    //         ERROR(4, "Failed to close file descriptor %d\n", fd_out);
+    //     }
+
+    //     execlp("file", "file", "-b", "E", filename, NULL);
+    //     break;
+
+    // default:
+    //     wait(NULL);
     // }
 
-    // FREE(files_list);
+    // fd_out = open(out_filename, O_RDONLY);
 
-    char *filename = args_info.file_arg[0];
-    FILE *fptr = NULL;
-    fptr = fopen(filename, "r");
+    // if (fd_out == -1)
+    // {
+    //     ERROR(7, "Failed to open outuput file");
+    // }
 
-    if (fptr == NULL)
-    {
-        ERROR(1, "cannot open file '%s'", filename);
-    }
+    // char str_type[5] = "";
+    // char buffer;
 
-    fclose(fptr);
+    // for (int i = 0; i < 5; ++i)
+    // {
+    //     if (read(fd_out, &buffer, 1) == -1)
+    //     {
+    //         ERROR(6, "Failed to read from output file");
+    //     }
+    //     if (buffer == 32)
+    //     {
+    //         break;
+    //     }
 
-    char *str_ext = strrchr(filename, 46);
-    char new_str_ext[(int)strlen(str_ext)];
+    //     str_type[i] += tolower(buffer);
+    // }
 
-    if (str_ext == NULL)
-    {
-        str_ext = "";
-        printf("[WARNING] '%s' filename does not have an extension\n", filename);
-    }
-    else
-    {
-        for (int i = 0; i < (int)strlen(str_ext); i++)
-        {
-            new_str_ext[i] = str_ext[i + 1];
-        }
-    }
+    // printf("%s\n", str_type);
 
-    int fd_out;
-    int fd_err;
+    // if (close(fd_out) == -1)
+    // {
+    //     ERROR(7, "Failed to close output file");
+    // }
 
-    char *out_filename = "./out.txt";
-    char *err_filename = "./err.txt";
+    // int comp;
+    // int j = 0;
+    // do
+    // {
+    //     comp = strcmp(str_type, EXTENSIONS[j]);
+    //     j++;
+    // } while (comp != 0 && j < 7);
 
-    pid_t pid = fork();
+    // if (comp != 0)
+    // {
+    //     printf("[INFO] '%s': type '%s' is not supported by checkFile\n", filename, str_type);
+    //     exit(1);
+    // }
 
-    switch (pid)
-    {
-    case -1:
-        ERROR(5, "fork failed\n");
-        break;
-    case 0:
-
-        fd_out = open(out_filename, O_CREAT | O_WRONLY | O_TRUNC | O_SYNC, 0777);
-
-        if (fd_out == -1)
-        {
-            ERROR(3, "Failed to create output file\n");
-        }
-
-        fd_err = open(err_filename, O_CREAT | O_WRONLY | O_TRUNC | O_SYNC, 0777);
-        if (fd_err == -1)
-        {
-            ERROR(3, "Failed to create error file\n");
-        }
-
-        dup2(fd_out, STDOUT_FILENO);
-        dup2(fd_err, STDERR_FILENO);
-
-        if (close(fd_out) == -1)
-        {
-            ERROR(4, "Failed to close file descriptor %d\n", fd_out);
-        }
-
-        if (close(fd_err) == -1)
-        {
-            ERROR(4, "Failed to close file descriptor %d\n", fd_err);
-        }
-
-        execlp("file", "file", "-b", filename, NULL);
-        break;
-
-    default:
-        wait(NULL);
-    }
-
-    fd_out = open(out_filename, O_RDONLY);
-
-    if (fd_out == -1)
-    {
-        ERROR(7, "Failed to open outuput file");
-    }
-
-    char str_type[5] = "";
-    char buffer;
-
-    for (int i = 0; i < 5; ++i)
-    {
-        if (read(fd_out, &buffer, 1) == -1)
-        {
-            ERROR(6, "Failed to read from output file");
-        }
-        if (buffer == 32)
-        {
-            break;
-        } 
-
-        str_type[i] += tolower(buffer);
-    }
-
-    printf("%s\n", str_type);
-
-    if (close(fd_out) == -1)
-    {
-        ERROR(7, "Failed to close output file");
-    }
-
-    int comp;
-    int j = 0;
-    do
-    {
-        comp = strcmp(str_type, EXTENSIONS[j]);
-        j++;
-    } while (comp != 0 && j < 7);
-
-    if (comp != 0)
-    {
-        printf("[INFO] '%s': type '%s' is not supported by checkFile\n", filename, str_type);
-        exit(1);
-    }
-
-    if (strcmp(str_type, new_str_ext) != 0)
-    {
-        printf("[MISMATCH] '%s': extension is '%s', file type is '%s'\n", filename, new_str_ext, str_type);
-    }
-    else
-    {
-        printf("[OK] '%s': extension '%s' matches file type '%s'\n", filename, new_str_ext, str_type);
-    }
+    // if (strcmp(str_type, new_str_ext) != 0)
+    // {
+    //     printf("[MISMATCH] '%s': extension is '%s', file type is '%s'\n", filename, new_str_ext, str_type);
+    // }
+    // else
+    // {
+    //     printf("[OK] '%s': extension '%s' matches file type '%s'\n", filename, new_str_ext, str_type);
+    // }
 
     // ler o out.txt para uma string e fazer a comparação com o filename e com o array de extensoes suportadas
 
     cmdline_parser_free(&args_info);
 
     return 0;
+}
+
+int file_check(char *filename)
+{
+    FILE *fptr = NULL;
+    fptr = fopen(filename, "r");
+
+    if (fptr == NULL)
+    {
+        fprintf(stderr, "[ERROR] cannot open file '%s' -- %s\n", filename, strerror(errno));
+        return 1;
+    }
+
+    fclose(fptr);
+    return 0;
+}
+
+// char **file_list(char **args, int n_args)
+// {
+//     int i;
+
+//     char **file_list = (char **)MALLOC((sizeof(char *)) * n_args);
+
+//     for (i = 0; i < n_args; ++i)
+//     {
+//         char *filename = (char *)MALLOC((sizeof(char)) * 100);
+//         strcpy(filename, args[i]);
+//         strcpy(file_list[i], filename);
+//         FREE(filename);
+//     }
+
+//     FREE(file_list);
+// }
+
+char *extract(char *filename)
+{
+    char *chr = strrchr(filename, 46);
+    int i;
+    char *str_ext = NULL;
+
+    if (chr == NULL)
+    {
+        printf("[WARNING] '%s' filename does not have an extension\n", filename);
+    }
+    else
+    {
+        int n_bytes = (int)strlen(chr) + 1;
+        str_ext = MALLOC(n_bytes);
+
+        for (i = 0; i < n_bytes; ++i)
+        {
+            str_ext[i] += chr[i + 1];
+        }
+    }
+
+    return str_ext;
 }
